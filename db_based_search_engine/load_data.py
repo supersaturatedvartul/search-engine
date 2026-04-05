bc466909e1977ab01c3a5067f318b8b0d4ec6564
supersaturatedvartul <bhujbalraoomkars@gmail.com>
2026-04-05 20:19:37 +0530

    'Search Engine v1.3'

--- db_based_search_engine/load_data.py
+++ db_based_search_engine/load_data.py
@@ -3,10 +3,14 @@ from db import connection
 conn = connection()
 
 cursor = conn.cursor()
-with open('webpage.txt','r') as file:
-    content = file.readlines()
-for i in content:
-    i = i.strip()
-    if i:
-        cursor.execute("INSERT into contentable (content) VALUES (%s)",(i.lower(),))
+liblist = ['c.txt','java.txt','python.txt']
+
+for i in liblist:
+    with open(f'library/{i}','r') as file:
+        content = file.readlines()
+    for j in content:
+        j = j.strip()
+        if j:
+            if len(j.split())>15:
+                cursor.execute("INSERT into contentable (content) VALUES (%s)",(j.lower(),))
 conn.commit()
